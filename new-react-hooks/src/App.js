997fadaf82dc4ea86c35d8fc661334d6f0cb1a08
blueprint-12 <blueprint12@daum.net>
2022-12-16 18:55:33 +0900

    feat: React.forwardRef test

---
+++ new-react-hooks/src/App.js
@@ -0,0 +1,22 @@
+import { useRef } from "react";
+import MyInput from "./MyInput";
+import "./App.css";
+
+function App() {
+  const inputRef = useRef();
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  // 그냥 props로 넘겨준다면, ref 는 props가 아니기 때문에 개발자도구에서 에러메세지를 띄운다.
+  return (
+    <div className="App">
+      {/* <input ref={inputRef} /> */}
+      <MyInput ref={inputRef} />
+      <button onClick={focus}>집중</button>
+    </div>
+  );
+}
+
+export default App;
